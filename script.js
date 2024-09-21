5846d5f3a38f88a8bd154f6f771510908b8de4fa
Sudip <sudipdasghosh64@gmail.com>
2024-09-21 10:28:30 +0530

    2ND DAY

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 // ============= LOADER =============
-var loader = document.querySelector(".loader");
+function loader() {
+    var loader = document.querySelector(".loader");
+    var main = document.querySelector('main');
+    
+    setTimeout(function load() {
+      loader.style.display = "none";
+      main.style.position = "relative";
+    }
+    , 2000);
+};
 
-setTimeout(function load() {
-  loader.style.display = "none";
-}
-, 2000);
+
+// <!-- Initialize Swiper -->
+  const progressCircle = document.querySelector(".autoplay-progress svg");
+  const progressContent = document.querySelector(".autoplay-progress span");
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 10000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    // on: {
+    //   autoplayTimeLeft(s, time, progress) {
+    //     progressCircle.style.setProperty("--progress", 1 - progress);
+    //     progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    //   }
+    // }
+  });
 
 // ============= MOBILE NAVBER =============
 function resNav(){
@@ -152,7 +183,7 @@ function review() {
 
 
 
-
+loader();
 resNav();
 review();
 textAnimation();
